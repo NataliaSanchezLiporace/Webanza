bbeb2d303066fb072a5c2bdbdbd123e40def4dd0
NataliaSanchezLiporace <74458085+NataliaSanchezLiporace@users.noreply.github.com>
2021-03-18 23:00:36 -0300

    Update ItemListContainer.js

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { ItemCount } from "../ItemCount";
 
 export default function ItemListContainer () {
-	return <div> Lista de Servicios </div> ;
-		
-	}
+	return <div> Lista de Servicios 
+	(<div className="container ">
+      Items List
+      <ItemCount stock="5" initial="1" />
+    </div>);		
+}
     
